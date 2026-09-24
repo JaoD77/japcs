272d6e94450c6eb28edbc7ce80a653e984d7f9b9
JaoD77 <jaopedroreis1701@gmail.com>
2026-09-24 14:02:09 -0300

    extract

--- japasta/adicionar_carrinho.php
+++ japasta/adicionar_carrinho.php
@@ -39,4 +39,45 @@ if(!isset($_SESSION['Logado']) || $_SESSION['Logado'] != 'ok'){
 $_SESSION['msg_vitrine'] = 'Produto adicionado ao carrinho!';
 header('Location: carrinho.php');
 exit;
+?><?php
+if(!isset($_SESSION)) session_start();
+
+if(!isset($_POST['b_adicionar'])){
+    header('Location: index.php');
+    exit;
+}
+
+extract($_POST);
+$produto_preco = floatval($produto_preco);
+
+if(!isset($_SESSION['carrinho'])) $_SESSION['carrinho'] = [];
+
+$ja_existe = false;
+foreach($_SESSION['carrinho'] as $indice => $item){
+    if($item['id'] == $produto_id){
+        $_SESSION['carrinho'][$indice]['quantidade'] = $item['quantidade'] + 1;
+        $ja_existe = true;
+        break;
+    }
+}
+
+if(!$ja_existe){
+    $_SESSION['carrinho'][] = [
+        'id'         => $produto_id,
+        'nome'       => $produto_nome,
+        'preco'      => $produto_preco,
+        'quantidade' => 1,
+    ];
+}
+
+if(!isset($_SESSION['Logado']) || $_SESSION['Logado'] != 'ok'){
+    $_SESSION['msg_login']          = 'Faça login para finalizar a compra.';
+    $_SESSION['redirect_apos_login'] = 'carrinho.php';
+    header('Location: login.php');
+    exit;
+}
+
+$_SESSION['msg_vitrine'] = 'Produto adicionado ao carrinho!';
+header('Location: carrinho.php');
+exit;
 ?>
